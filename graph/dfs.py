d5c16f5377486ddb9be4b2472dae4fa943196388
Tarun-asati21 <tarunasati96@gmail.com>
2026-09-21 11:53:11 +0530

    Graph : DFS understood using recursive approach !!

---
+++ graph/dfs.py
@@ -0,0 +1,27 @@
+#  recursive approach code
+
+def dfs(node, result, visited, adj) :
+    visited[node]=1
+    result.append(node)
+    for n in adj[node]:
+        if visited[n]==0 :
+            dfs(n, result, visited, adj)
+
+number_of_nodes = 8
+adj = [
+    [],
+    [2,4],
+    [1,3,6],
+    [2],
+    [1,5,7],
+    [4,8],
+    [2],
+    [4,8],
+    [5,7]
+]
+
+visited = [0]*(number_of_nodes +1)
+result = []
+dfs(1, result, visited, adj)
+
+print(result)
